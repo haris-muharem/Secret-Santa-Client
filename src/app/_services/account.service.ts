59b6c1609f6175e8be16f391cdf945712276e1a8
harisbegic2001 <harisbegic01@hotmail.com>
2024-01-20 01:04:10 +0100

    promjena na local

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -8,7 +8,7 @@ import { User } from '../_models/user';
   providedIn: 'root'
 })
 export class AccountService {
-baseUrl = 'http://18.194.173.91/api/';
+baseUrl = 'http://3.71.92.233:8080/api/';
 
 private currentUserSource = new BehaviorSubject<User | null>(null);
 currentUser$ = this.currentUserSource.asObservable();
